ff4a3703afc4f2c522b324f2a5c1c49cc15f31a4
marco <28888207+Marco3jp@users.noreply.github.com>
2018-07-25 03:54:28 +0900

    グローバル変数の定義用ファイルを作り、エンドポイント周りを定義した

---
+++ src/www/js/global.js
@@ -0,0 +1,8 @@
+let host = "http://localhost";
+let port = "9250";
+let apiPrefix = "/api";
+
+let configEndpoint = "/config";
+let genreEndpoint = "/genreList";
+let todoEndpoint = "/todoList";
+let solutionEnpoint = "/solution";
