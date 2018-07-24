9c63b81c4e44d3116fbc6e29f31b784d24a5485a
marco <28888207+Marco3jp@users.noreply.github.com>
2018-07-24 19:05:45 +0900

    main.goを書ききった

--- src/main.go
+++ src/main.go
@@ -1,1 +1,41 @@
 package main
+
+import (
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
+
+	"fmt"
+	"net/http"
+
+	"./router"
+	"./model"
+	"encoding/json"
+	"io/ioutil"
+)
+
+func main()  {
+	fmt.Printf("Start API Server!\n")
+
+	configFile ,err := ioutil.ReadFile("./config.json")
+	if err!=nil{
+		fmt.Printf("There is not config file! :o\n")
+		panic(err)
+	}
+
+	config := new(model.ApplicationConfig)
+
+	if err = json.Unmarshal(configFile,config);err != nil{
+		fmt.Printf("this is false config file :(\n")
+		panic(err)
+	}
+
+	db , dbErr := sql.Open("sqlite3", config.DataBaseDirectory)
+	if dbErr != nil {
+		fmt.Printf("wrong directory or cannot read db file :<\n")
+		panic(dbErr)
+	}
+	defer db.Close()
+
+	r := router.Init(db)
+	http.ListenAndServe(":9250", r)
+}
