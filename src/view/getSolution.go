75520aada4e194e8cbda392bb386b697d54aae64
marco <28888207+Marco3jp@users.noreply.github.com>
2018-07-27 08:26:35 +0900

    ACAOをすべてにおいて*にセットした

--- src/view/getSolution.go
+++ src/view/getSolution.go
@@ -6,6 +6,7 @@ import (
 )
 
 func GetSolution(solutionJson *[]byte,w http.ResponseWriter)  {
+    w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 
